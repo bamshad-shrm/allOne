962d41275eca5a1c2ca85439ed3fba5462c2a6e7
bamshad-shm <bamshad.shm@gmail.com>
2020-10-08 22:28:41 -0400

    first graphs

--- analyzer.R
+++ analyzer.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # This file go through all the LOD files, calculate the average of the socres and make the graphs
 
 # Settings:
@@ -15,7 +17,9 @@ groupModes = c(0,1)
 # Create a data frame for our average scores
 averageOfScores <- data.frame("replicateNumber" = replicateNumbers)
 
+
 # Going through all the files
+clmNumber = 2
 for (rewardMode in rewardModes) {
   for (groupMode in groupModes) {
     #cat(paste(filesFolder, "LOD_",rewardMode,"_",groupMode, sep = ""),"\n")
@@ -27,12 +31,23 @@ for (rewardMode in rewardModes) {
       #cat(paste(filesFolder, "LOD_",rewardMode,"_",groupMode,"_",replicateNumber, sep = ""),"mean socre: ",mean(currentLodFile$score),"\n")
       averageOfScores[replicateNumber+1,newClmnName] = mean(currentLodFile$score)
     }
+    # Plot the  scatter plot
+    newClmnName <- ggplot(averageOfScores, aes(averageOfScores$replicateNumber,averageOfScores[,clmNumber])) +
+      geom_point()+
+      geom_path()+
+      xlab("Replicate Number")+
+      ylab("Average Of Scores")+
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+      ggtitle(newClmnName)+
+      ylim(0, 150)
+    
+    # Save the sector graph
+    graphName =  paste(clmNumber, ".png", sep = "")
+    ggsave(graphName, newClmnName)
+    clmNumber = clmNumber + 1
   }
 }
 
 print(averageOfScores)
-
-
- 
-
 
