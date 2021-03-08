7075e309e043bb99d64818301bf9c6b920bc0551
bamshad-shm <bamshad.shm@gmail.com>
2021-03-08 16:10:20 +0330

    removing extra files and folders

---
+++ methods/test/beepAnalyzer.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt 
+import pandas as pd
+from scipy import stats
+
+# Settings:
+# Folder's path of the LOD files.
+filesFolder="C:/Users/blue/Documents/study/BI/Thesis/allOne/methods/test/data/beep/"
+# Folder's path of the lod statistics
+beepStatisticsFolder="C:/Users/blue/Documents/study/BI/Thesis/allOne/methods/test/results/beepStatistics/"
+# number of replicate
+numberOfReplicate = 99
+replicateNumbers = range(numberOfReplicate+1)
+# reward modes:
+rewardModes = [0,1,2,3]
+# group modes:
+groupModes = [0,1]
+# End of settings
+
+
+bValues = []
+bMinimums = []
+bMaximums = []
+bAverages = []
+bSums = []
+
+mValues = []
+mMinimums = []
+mMaximums = []
+mAverages = []
+mSums = []
+
+
+# 1. Adding the minimum, maximum, sum and average of each row for beep and muted cases.
+
+for rewardMode in rewardModes:
+    for groupMode in groupModes:
+        for replicateNumber in replicateNumbers:
+            fileName = "beep"+"_"+str(rewardMode)+"_"+str(groupMode)+"_"+str(replicateNumber)
+            df = pd.read_csv(filesFolder+fileName+".csv")
+            for i in range(numberOfReplicate+1):
+                bValues.append(df["b0"][i])
+                bValues.append(df["b1"][i])
+                bValues.append(df["b2"][i])
+                bValues.append(df["b3"][i])
+                bMinimums.append(min(bValues))
+                bMaximums.append(max(bValues))
+                bAverages.append(sum(bValues)/4)
+                bSums.append(sum(bValues))
+                bValues.clear()
+                mValues.append(df["m0"][i])
+                mValues.append(df["m1"][i])
+                mValues.append(df["m2"][i])
+                mValues.append(df["m3"][i])
+                mMinimums.append(min(mValues))
+                mMaximums.append(max(mValues))
+                mAverages.append(sum(mValues)/4)
+                mSums.append(sum(mValues))
+                mValues.clear()
+
+            df['mMinimumScore'] = mMinimums
+            df['bMinimumScore'] = bMinimums
+            df['mMaximumScore'] = mMaximums
+            df['bMaximumScore'] = bMaximums
+            df['mAverageOfScores'] = mAverages
+            df['bAverageOfScores'] = bAverages
+            df['mSumOfScores'] = mSums
+            df['bSumOfScores'] = bSums
+            
+            df.to_csv(filesFolder+fileName+".csv", index=False)
+            bMinimums.clear()
+            bMaximums.clear()
+            bAverages.clear()
+            bSums.clear()
+            mMinimums.clear()
+            mMaximums.clear()
+            mAverages.clear()
+            mSums.clear()
+
+
+
+# 2. Calculation of the mean of minimum, maximum, sum and average of all the replicates of each condition.
+
+allReplicates = pd.DataFrame (columns = ['replicate','m0','m1','m2','m3','b0','b1','b2','b3',
+                                         'mMinimumScore','bMinimumScore','mMaximumScore','bMaximumScore',
+                                         'mSumOfScores','bSumOfScores','mAverageOfScores','bAverageOfScores',
+                                         'MinimumsKs','MaximumsKs','AveragesKs','SumsKs'])
+
+rewardGroupModes = []
+
+bMeanMinimums = []
+bMeanMaximums = []
+bMeanAverages = []
+bMeanSums = []
+
+mMeanMinimums = []
+mMeanMaximums = []
+mMeanAverages = []
+mMeanSums = []
+
+MinimumsKs = []
+MaximumsKs = []
+AveragesKs = []
+SumsKs = []
+
+for rewardMode in rewardModes:
+    for groupMode in groupModes:
+        for replicateNumber in replicateNumbers:
+            fileName = "beep"+"_"+str(rewardMode)+"_"+str(groupMode)+"_"+str(replicateNumber)
+            df = pd.read_csv(filesFolder+fileName+".csv")
+            allReplicates = allReplicates.append(df)
+        rewardGroupModes.append("beep"+"_"+str(rewardMode)+"_"+str(groupMode))
+        bMeanMinimums.append(sum(allReplicates["bMinimumScore"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        bMeanMaximums.append(sum(allReplicates["bMaximumScore"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        bMeanAverages.append(sum(allReplicates["bAverageOfScores"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        bMeanSums.append(sum(allReplicates["bSumOfScores"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        mMeanMinimums.append(sum(allReplicates["mMinimumScore"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        mMeanMaximums.append(sum(allReplicates["mMaximumScore"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        mMeanAverages.append(sum(allReplicates["mAverageOfScores"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+        mMeanSums.append(sum(allReplicates["mSumOfScores"])/((numberOfReplicate+1)*(numberOfReplicate+1)))
+
+        MinimumsKs.append(stats.ks_2samp(allReplicates["bMinimumScore"],allReplicates["mMinimumScore"]))
+        MaximumsKs.append(stats.ks_2samp(allReplicates["bMaximumScore"],allReplicates["mMaximumScore"]))
+        AveragesKs.append(stats.ks_2samp(allReplicates["bAverageOfScores"], allReplicates["mAverageOfScores"]))
+        SumsKs.append(stats.ks_2samp(allReplicates["bSumOfScores"], allReplicates["mSumOfScores"]))
+
+        allReplicates.drop(allReplicates.index, inplace=True)
+
+dict = {'rewardGroupModes': rewardGroupModes,
+        'bMeanMinimums': bMeanMinimums,
+        'bMeanMaximums': bMeanMaximums,
+        'bMeanAverages': bMeanAverages,
+        'bMeanSums': bMeanSums,
+        'mMeanMinimums': mMeanMinimums,
+        'mMeanMaximums': mMeanMaximums,
+        'mMeanAverages': mMeanAverages,
+        'mMeanSums': mMeanSums,
+        'MinimumsKs' : MinimumsKs,
+        'MaximumsKs' : MaximumsKs,
+        'AveragesKs' : AveragesKs,
+        'SumsKs' : SumsKs
+}  
+    
+beepStatistics = pd.DataFrame(dict)
+
+#print(beepStatistics)
+
+beepStatistics.to_csv(beepStatisticsFolder+"beepStatistics"+".csv", index=False)
+
+
+
+
